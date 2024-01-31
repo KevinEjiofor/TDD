0f599e3559cd47cd2a957d553db531ef8ca70856
KevinEjiofor <ejioforkelvin@gmail.com>
2024-01-31 20:17:25 +0100

    updated files

--- src/hackerRank/Permutations.java
+++ src/hackerRank/Permutations.java
@@ -1,35 +1,39 @@
 package hackerRank;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Permutations {
     public static void main(String[] args) {
-            Permutations permutationsObj = new Permutations();
-            String originalString = "abc";
-            String modifiedString = permutationsObj.swapCharacters(originalString);
-            System.out.println("Original: " + originalString);
-            System.out.println("Modified: " + modifiedString);
-
+        Permutations permutationsObj = new Permutations();
+        String originalString = "cab";
+        List<String> result = permutationsObj.generatePermutations(originalString);
+        System.out.println("Original: " + originalString);
+        System.out.println("Permutations: " + result);
     }
 
-    public String swapCharacters(String permutations) {
+    public List<String> generatePermutations(String input) {
+        List<String> permutations = new ArrayList<>();
+        generatePermutationsHelper(new StringBuilder(input), 0, permutations);
+        return permutations;
+    }
 
-        if (permutations == null || permutations.length() < 2) {
-            return permutations;
+    private void generatePermutationsHelper(StringBuilder input, int start, List<String> permutations) {
+        if (start == input.length() - 1) {
+            permutations.add(input.toString());
+            return;
         }
 
-
-        StringBuilder channel = new StringBuilder(permutations);
-
-        char temp = channel.charAt(0);
-        channel.setCharAt(0, channel.charAt(1));
-        channel.setCharAt(1, temp);
-
-        return channel.toString();
+        for (int i = start; i < input.length(); i++) {
+            swap(input, start, i);
+            generatePermutationsHelper(input, start + 1, permutations);
+            swap(input, start, i);
+        }
     }
 
-
-
-
-
-
-
+    private void swap(StringBuilder input, int i, int j) {
+        char temp = input.charAt(i);
+        input.setCharAt(i, input.charAt(j));
+        input.setCharAt(j, temp);
+    }
 }
